610e926e0187f9b7b7431ed1e0f16465aa65150f
Almog Ben Yamin <almoggx@gmail.com>
2024-04-01 16:09:30 +0300

    Second Commit

---
+++ src/Login.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Login.css';
+import logoImage from "./assets/LOGO1.png";
+
+function Login() {
+  return (
+    <div className="login-container">
+    <div className="left-panel">
+      <div className="logo">
+      <img src= {logoImage} alt ="logo" />
+        <h1>LENDLOOP</h1>
+      </div>
+      <div className="sign-in-panel">
+        <h2>Sign in to dashboard</h2>
+      <form>
+        <div className="input-container">
+          <input type="text" placeholder="Username / Email" />
+        </div>
+        <div className="input-container">
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="button-container">
+  <button className="btn" type="submit">Sign In</button>
+  <button className="reg" type="button">Register</button>
+
+</div>
+<a href="#" className="login-link">Can't login? Click here!</a>  
+      </form>
+    </div>
+    </div>
+    <div className="right-panel">
+    <h2>Join Our Community</h2>
+        <p>LENDLOOP offers an innovative experience in the field of item lending. Made for users of all kinds, with no need to possess a social network account. LENDLOOP offers an ease-of-use not seen before with it's intuitive yet simple layout and plenty of quality of life features not seen in other websites!</p>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default Login;
---
+++ src/Register.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Register.css';
+import logoImage from "./assets/LOGO1.png";
+
+function Register() {
+    return (
+        <div className="register-container">
+            <div className="panel">
+                <div className="logo">
+                    <img src={logoImage} alt="logo" />
+                    <h1>LENDLOOP</h1>
+                </div>
+                <div className="register-panel">
+                    <h2>New user? Sign-up below!</h2>
+                    <form>
+                        <div className="input-container">
+                            <input type="text" placeholder="Username" />
+                        </div>
+                        <div className="input-container">
+                            <input type="email" placeholder="Email" />
+                        </div>
+                        <div className="input-container">
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <hr /> 
+                        <div className="input-container">
+                            <input type="tel" placeholder="Phone Number" />
+                        </div>
+                        <div className="input-container">
+                            <input type="number" placeholder="Age" />
+                        </div>
+                        <div className="button-container">
+                            <button className="btn" type="button">Sign-Up</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
